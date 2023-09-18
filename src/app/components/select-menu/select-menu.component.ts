daf6e228c1e78d25c075bf62467ba3b04765a148
Muhammad Ahmed Ejaz <ahm.ejaz791@gmail.com>
2023-09-18 20:54:50 +0500

    select menu

--- src/app/components/select-menu/select-menu.component.ts
+++ src/app/components/select-menu/select-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-select-menu',
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./select-menu.component.scss']
 })
 export class SelectMenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor() {}
+  ngOnInit() {}
 
 }

20b194d3f823558fe58b5172c9bfa345b7b9035a
Muhammad Ahmed Ejaz <ahm.ejaz791@gmail.com>
2023-10-03 02:26:09 +0500

    feedback and final touches

--- src/app/components/material-date-picker/material-date-picker.component.ts
+++ src/app/components/material-date-picker/material-date-picker.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit, Input } from '@angular/core';
 export class MaterialDatePickerComponent implements OnInit {
   @Input() label: any;
   @Input() required: boolean;
+  datePickerDocument: any;
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
